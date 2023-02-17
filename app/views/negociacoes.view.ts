9cc04e79da01ba9f0ecef478e5f54187bcf30542
Marione-Pereira <mari.tainara@gmail.com>
2023-02-17 13:54:03 -0300

    creating negotiation template and statting second course

---
+++ app/views/negociacoes.view.ts
@@ -0,0 +1,23 @@
+export class NegociacoesView {
+  private elemento : HTMLElement;
+  constructor(seletor : string){
+    this.elemento = document.querySelector(seletor)
+  }
+  template(): string {
+    return `
+      <table class ="table table-hover table-bordered">
+        <thead>
+          <tr>
+            <th>DATA</th>
+            <th>QUANTIDADE</th>
+            <th>VALOR</th>
+          </tr>
+        </thead>
+        <tbody></tbody>
+      </table>
+    `;
+  }
+  updated(): void {
+    this.elemento.innerHTML = this.template();
+  }
+} 
